95dc03fba3b8d54e8d0d24b4849f5aea1c76e75c
Nishan <nishantkhatiwada012@gmail.com>
2023-05-29 09:45:15 +0545

    new changes to add registration form

--- nishanblogweb/blogwebsite/models.py
+++ nishanblogweb/blogwebsite/models.py
@@ -1,6 +1,10 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -23,3 +27,10 @@ class Article(models.Model):
     def __str__(self):
         return self.title
     
+            
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
--- nishanblogweb/blogwebsite/views.py
+++ nishanblogweb/blogwebsite/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from .models import Article
+from .models import RegistrationForm
 
 # Create your views here.
 class ArticleListView(ListView):
@@ -10,3 +11,18 @@ class ArticleListView(ListView):
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'detail.html'
+    
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            print("Invalid Details")
+    else:
+        form = RegistrationForm
+    return render(request, 'register.html', {'form': form})
+        
+            
+    
